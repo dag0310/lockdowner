8c79d1fbe5eec34acdee68accf9a153c3b35e8ee
Daniel Geymayer <dag0310@gmail.com>
2021-01-30 15:38:32 +0100

    Update extraction script to prepare minimal JSON data for graph with date and 7 day incidency

--- extract_data.py
+++ extract_data.py
@@ -1,22 +1,32 @@
 import sys
 import os
+import csv
+import json
 import urllib.request
 
 if len(sys.argv) < 2:
-    print("Country ISO code argument missing, e.g. 'AUT'")
+    print("Land oder Bundesland fehlt, zB 'Österreich'")
     quit()
 
-country = sys.argv[1].upper()
-file_in_name = 'owid-covid-data.csv'
-file_out_name = 'owid-covid-data-' + country + '.csv'
+location = sys.argv[1]
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+csv_url = "https://covid19-dashboard.ages.at/data/CovidFaelle_Timeline.csv"
 
-urllib.request.urlretrieve("https://covid.ourworldindata.org/data/" + file_in_name, os.path.join(os.path.dirname(__file__), file_in_name))
+print('Downloading ' + csv_url)
+req = urllib.request.Request(csv_url, data=None, headers={'User-Agent': user_agent})
+csv_text = str(urllib.request.urlopen(req).read().decode('utf-8'))
 
-with open(os.path.join(os.path.dirname(__file__), file_in_name), 'r') as file_in:
-    with open(os.path.join(os.path.dirname(__file__), file_out_name), 'w') as file_out:
-        is_first_line = True
-        for line in file_in:
-            if line[0:3] == country or is_first_line:
-                if is_first_line:
-                    is_first_line = False
-                file_out.write(line)
+entries = []
+csv_reader = csv.reader(csv_text.split('\n'), delimiter=';')
+next(csv_reader)  # skip header row
+for row in csv_reader:
+    if row[1].lower() != location.lower():
+        continue
+    ddmmyyyy = row[0].split(' ')[0].split('.')
+    entries.append({
+        'date': ddmmyyyy[2] + '-' + ddmmyyyy[1] + '-' + ddmmyyyy[0],
+        'sevenDayIncidency': float(row[7].replace(',', '.'))
+    })
+
+with open(os.path.join(os.path.dirname(__file__), 'covid-austria.json'), 'w') as file_out:
+    file_out.write(json.dumps(entries, indent=2))
