73b6b0acbe388dcd64ed4ade76330e09d77a4589
Daniel Geymayer <dag0310@gmail.com>
2021-01-30 17:21:41 +0100

    Remove command line argument for location because umlauts are still hard apparently ...

--- extract_data.py
+++ extract_data.py
@@ -1,14 +1,9 @@
-import sys
 import os
 import csv
 import json
 import urllib.request
 
-if len(sys.argv) < 2:
-    print("Land oder Bundesland fehlt, zB 'Österreich'")
-    quit()
-
-location = sys.argv[1]
+location = 'Österreich'
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 csv_url = "https://covid19-dashboard.ages.at/data/CovidFaelle_Timeline.csv"
 
